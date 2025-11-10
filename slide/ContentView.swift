397e64ea7dd5ec21772120702de49294d8322459
GerardJalonG <gerard.jalon@gmail.com>
2025-11-10 16:52:25 +0100

    Create view Slider and modify values

--- slide/ContentView.swift
+++ slide/ContentView.swift
@@ -3,41 +3,39 @@ import SwiftUI
 struct ContentView: View {
     @State private var sliderValue: Double = 50
     @State private var target: Int = Int.random(in: 1...100)
-    @State private var message: String = ""
     @State var alertIsVisible: Bool = false
     
     var body: some View {
-        
-        VStack(spacing: 20) {
-            Text("🎯 Acierta el número")
-                .font(.largeTitle)
-            
-            Text("Tu objetivo: \(target)")
-                .font(.title2)
-            
-            // Slider
-            HStack {
-                Text("1")
-                Slider(value: $sliderValue, in: 1...100)
-                Text("100")
-            }.padding(.horizontal)
-            
-            // Botón para probar suerte
-            Button("Probar") {
-                alertIsVisible = true
+        ZStack{
+            Color("BackgroundColor")
+                            .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Text("🎯 Acierta el número")
+                    .font(.largeTitle)
+                
+                Text("Tu objetivo: \(target)")
+                    .font(.title2)
+                
+                // Slider
+                SliderView(value: sliderValue, lowValue: 1, highValue: 100)
+                
+                // Botón para probar suerte
+                Button("Probar") {
+                    alertIsVisible = true
+                }
+                .padding()
+                .background(Color.accentColor)
+                .foregroundColor(Color.white)
+                .cornerRadius(12)
+                .alert(isPresented: $alertIsVisible){
+                    Alert(title: Text("Hello"),
+                          message: Text("This is my first alert"),
+                          dismissButton: .default(Text("Alert is Close")))
+                }
+                
             }
             .padding()
-            .background(Color.accentColor)
-            .foregroundColor(Color.white)
-            .cornerRadius(12)
-            .alert(isPresented: $alertIsVisible){
-                Alert(title: Text("Hello"),
-                      message: Text("This is my first alert"),
-                      dismissButton: .default(Text("Alert is Close")))
-            }
-            
         }
-        .padding()
     }
     
 }
@@ -46,6 +44,20 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+                .previewDevice("iPhone 11 Pro")
         }
+    }
+}
+
+struct SliderView: View {
+    @State var value: Double
+    let lowValue: Double
+    let highValue: Double
+    var body: some View {
+        HStack {
+            Text("\(Int(lowValue))")
+            Slider(value: $value, in: lowValue...highValue)
+            Text("\(Int(highValue))")
+        }.padding(.horizontal)
     }
 }
